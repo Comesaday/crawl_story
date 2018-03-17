3dc6898abe0368d79fd8e4056b027b19e955aa8b
Comesaday <931716599@qq.com>
2018-03-17 22:08:53 +0800

    Climb a novel

--- demo3/get_Storys.py
+++ demo3/get_Storys.py
@@ -1,10 +1,8 @@
 #-*- coding:utf-8 -*-
 from urllib import request 
 from bs4 import BeautifulSoup
-import threading
 import os
 import re
-import time
 
 def get_html(url):
     #user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
